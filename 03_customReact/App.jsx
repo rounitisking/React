96b2e7a6aabc30f4ff515cba6223d3980db6a546
Rounit Singh <rounitsingh1405@gmail.com>
2025-09-11 15:24:02 +0530

    here i have written a code in which i am creating a custom react with its injecting fucntionality and also i am here showing how react treat the jsx

---
+++ 03_customReact/App.jsx
@@ -0,0 +1,9 @@
+const App = function(){
+    return (
+        <>
+        <h1>rounit singh :: this msg is from our custome react folder</h1>
+        </>
+    )
+}
+
+export default App
---
+++ 03_customReact/main.jsx
@@ -0,0 +1,45 @@
+// ham yaha pr yeh dekh rhe hai ki jab ham ko function ke andar jsx return krte hai tho react usse kaise dekhta hai , ham ussi tarah usse main mai paas karenge
+
+
+function customRender(reactEle,container){
+
+    //element is created 
+    const domEle = document.createElement(reactEle.type)
+
+
+    // here i am assigning the values in the dom created 
+    // domEle.setAttribute('href'  , reactEle.props.href) 
+    // domEle.setAttribute('target'  , reactEle.props.target) 
+    // domEle.target = reactEle.props.target
+    // domEle.innerHtml = reactEle.children
+
+
+    // now i am injecting the created dom element in the html
+
+    container.appendChild(domEle)
+}
+
+import App from "./App.jsx"
+import { StrictMode } from "react"
+import {createRoot} from 'react-dom/client'
+
+// writing the elements like the react interpret them 
+const reactEle = {
+    type : 'a',
+    props : {
+        href : 'http://google.com',
+        target : "_blank"
+    },
+    children : 'click me to visit google'
+}
+
+const container = document.getElementById('root')
+
+customRender(reactEle ,container)
+
+
+// createRoot.document.getElementById('root').render(
+//     <StrictMode>
+//         <App/>
+//         </StrictMode>
+// )
