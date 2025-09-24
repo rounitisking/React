2053ec6e3d835ed0ab656bb2cf17230692afe15b
Rounit Singh <rounitsingh1405@gmail.com>
2025-09-24 12:04:35 +0530

    creating custom hooks

---
+++ 02_projects/03_customHooks_Project/currency_Project/src/App.jsx
@@ -0,0 +1,17 @@
+// in this project we will be learning about the api calls and fetching the data from it 
+
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     
+    </>
+  )
+}
+
+export default App
---
+++ 02_projects/03_customHooks_Project/currency_Project/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,20 @@
+//defining the custom hooks
+
+import { useEffect , useState } from "react";
+
+
+function useCurrecyInfo (currencyInfo){
+    const [data , setData] = useState({})
+    useEffect(()=>{
+            fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currencyInfo}.json`)
+            .then(res => res.json)
+            .then(res => {
+                return setData(res[currencyInfo])
+            })
+    } , [currencyInfo])
+
+    return data
+}
+
+export default useCurrecyInfo
+
